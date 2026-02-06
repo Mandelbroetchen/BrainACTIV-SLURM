84d6054a1c4a850587da215191f03b45cedf8d14
Mandelbroetchen <anqiauschina@gmail.com>
2026-02-06 07:22:26 +0100

    downloads

--- importations.py
+++ importations.py
@@ -4,11 +4,7 @@ from PIL import Image
 import numpy as np
 from diffusers import DDIMScheduler, StableDiffusionImg2ImgPipeline
 from ip_adapter import IPAdapter
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
-import sys
-sys.path.append('/content/BrainACTIV')
-from BrainACTIV.dataset.nsd_clip import CLIPExtractor
-from BrainACTIV.methods.dino_encoder import EncoderModule, DINO_TRANSFORM
-from BrainACTIV.methods.slerp import slerp
+from dataset.nsd_clip import CLIPExtractor
+from methods.dino_encoder import EncoderModule, DINO_TRANSFORM
+from methods.slerp import slerp
